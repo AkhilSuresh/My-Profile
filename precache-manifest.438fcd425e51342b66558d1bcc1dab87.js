ca0799dd3655fa9dafc6854efbfa22b0b364656a
Akhil888 <akhils@qburst.com>
2020-11-23 09:53:58 +0100

    Updates

---
+++ precache-manifest.438fcd425e51342b66558d1bcc1dab87.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "744e353b5df874da9e86",
+    "url": "/My-Profile/static/js/runtime~main.744e353b.js"
+  },
+  {
+    "revision": "ebdb62dfd4c083ad1b9b",
+    "url": "/My-Profile/static/js/main.ebdb62df.chunk.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/My-Profile/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "ebdb62dfd4c083ad1b9b",
+    "url": "/My-Profile/static/css/main.ebef78f3.chunk.css"
+  },
+  {
+    "revision": "991c96cc6cd7d6a82318f93fd37a3451",
+    "url": "/My-Profile/index.html"
+  }
+];
